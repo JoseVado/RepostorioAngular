8b0d8b3f72695558cbe5a54bba57fb2338d03dec
vado <86899022+JoseVado@users.noreply.github.com>
2022-10-17 22:36:45 -0500

    segunda sesion angular despues de calculadora

--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -10,10 +10,12 @@ import { Component } from "@angular/core";
 export class PersonasComponent{
     deshabilitar = false;
 
-    mensaje = "No se ha agregado una persona";
+    mensaje = "";
     titulo = "default";
+    mostrar = false;
 
     agregarPersona() {
+        this.mostrar = true;
         this.mensaje = "Persona agregada";
     }
     /*
